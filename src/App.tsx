b57795bf41b6ee0fe03fe356ca97a8ede1f27603
Roya Rastegar <rOYA.RASTEGAR54196@GMAIL.COM>
2024-05-15 13:41:16 +0800

    write addto cart,get totalitems in app component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useQuery } from "react-query";
 // components
 import Item from "./Item/Item";
+import Cart from "./Cart/Cart";
 import Drawer from "@mui/material/Drawer";
 import LinearProgress from "@mui/material/LinearProgress";
 import Grid from "@mui/material/Grid";
@@ -9,7 +10,7 @@ import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import Badge from "@mui/material/Badge";
 
 // styles
-import { Wrapper } from "./App.styled";
+import { StyledButton, Wrapper } from "./App.styled";
 import { promises } from "dns";
 
 // types
@@ -19,21 +20,56 @@ const getProducts = async (): Promise<CartItemType[]> =>
   await (await fetch("http://fakestoreapi.com/products")).json();
 
 const App = () => {
+  const [cartOpen, setCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([] as CartItemType[]);
   const { data, isLoading, error } = useQuery<CartItemType[]>(
     "products",
     getProducts
   );
-  console.log(data);
-  const getTotalItems = () => null;
-  const handelAddToCart = (clickedItem: CartItemType) => null;
+
+  const getTotalItems = (items: CartItemType[]) =>
+    items.reduce((ack: number, item) => ack + item.amount, 0);
+
+  const handelAddToCart = (clickedItem: CartItemType) => {
+    setCartItems((prev) => {
+      // is the item already add to cart?
+      const isItemInCart = prev.find((item) => item.id === clickedItem.id);
+      if (isItemInCart) {
+        prev.map((item) =>
+          item.id === clickedItem.id
+            ? { ...item, amount: item.amount + 1 }
+            : item
+        );
+      }
+      // first time the item is add
+      return [...prev, { ...clickedItem, amount: 1 }];
+    });
+  };
+
   const handelRemoveFromCart = () => null;
+
   if (isLoading) return <LinearProgress />;
   if (error) return <div>somthing went error ...</div>;
   return (
     <Wrapper>
+      <Drawer anchor="right" open={cartOpen} onClose={() => setCartOpen(false)}>
+        <Cart
+          cartItems={cartItems}
+          addToCart={handelAddToCart}
+          removeFromCart={handelRemoveFromCart}
+        />
+      </Drawer>
+      <StyledButton onClick={() => setCartOpen(true)}>
+        <Badge badgeContent={getTotalItems(cartItems)} color="error">
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
+
       <Grid container spacing={3}>
         {data?.map((item) => (
-          <Grid item key={item.id} xs={12} sm={4}></Grid>
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handelAddToCart={handelAddToCart} />
+          </Grid>
         ))}
       </Grid>
     </Wrapper>
